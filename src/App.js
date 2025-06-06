f8b6022e3d12df560441033064371fb0e2862647
Mateusz Matysek <mateusziom1@gmail.com>
2025-06-06 17:20:28 +0200

    Update website to use modules

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-
-function App() {
+import Hero from './components/Hero/Hero'
+import SearchForm from './components/SearchForm/SearchForm'
+import List from './components/List/List'
+import Container from './components/Container/Container';
+const App = () => {
   return (
-    <div className="App">
-
-    </div>
+    <Container>
+      <Hero />
+      <SearchForm />
+      <List />
+    </Container>
   );
-}
+};
 
 export default App;
