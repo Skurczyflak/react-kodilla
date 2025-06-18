b2154b44710546d261170172c43464ccda52d5cf
Mateusz Matysek <mateusziom1@gmail.com>
2025-06-18 16:39:09 +0200

    Change 'db' to use Redux

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,44 +1,43 @@
 import styles from './List.module.scss';
 import Column from '../Column/Column';
 import ColumnForm from '../ColumnForm/ColumnForm';
-import shortid from 'shortid';
-import { data } from '../../db/data';	
-import { useState } from 'react';
+import { useSelector } from 'react-redux';
 
 const List = () => {
 
-const [columns, setColumns] = useState(data);
+  const columns = useSelector(state => state.columns);
 
-const addCard = (newCard, columnId) => {
-	const columnsUpdated = columns.map(column => {
-		if(column.id === columnId)
-			return { ...column, cards: [...column.cards, { id: shortid(), title: newCard.title }]}
-		else
-			return column
-	})
-
-	setColumns(columnsUpdated);
-
-};
-
-const addColumn = newColumn => {
-    setColumns([...columns, { id: shortid(), title: newColumn.title, icon: newColumn.icon, cards: [], addCard: addCard }]);
-};
-
-
+    // return (
+    //   <div className={styles.list}>
+    //     <header className={styles.header}>
+    //         <h2 className={styles.title}>Things to do <span>soon</span></h2>
+    //     </header>
+    //     <p className={styles.description}>Interesting things I want to check out</p>
+    //     <section className={styles.columns}>
+    //       {columns.map(column => <Column key={column.id} id={column.id} title={column.title} icon={column.icon} cards={column.cards} addCard={addCard} />)}
+    //     </section>
+    //     <ColumnForm handleSubmit={addColumn} />
+    //   </div>
+    // );
 
     return (
-      <div className={styles.list}>
-        <header className={styles.header}>
-            <h2 className={styles.title}>Things to do <span>soon</span></h2>
-        </header>
-        <p className={styles.description}>Interesting things I want to check out</p>
-        <section className={styles.columns}>
-          {columns.map(column => <Column key={column.id} id={column.id} title={column.title} icon={column.icon} cards={column.cards} addCard={addCard} />)}
-        </section>
-        <ColumnForm handleSubmit={addColumn} />
-      </div>
+    <div className={styles.list}>
+      <header className={styles.header}>
+        <h2 className={styles.title}>Things to do<span>soon!</span></h2>
+      </header>
+      <p className={styles.description}>Interesting things I want to check out</p>
+      <section className={styles.columns}>
+        {columns.map(column =>
+          <Column
+            key={column.id}
+            {...column}  />
+        )}
+      </section>
+      <ColumnForm />
+    </div>
     );
+
+
   };
 
   export default List;
